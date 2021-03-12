1a191f54e2dad7cbe84a1d06cd94bab6a4a295b8
William <wiltorc_182@hotmail.com>
2021-03-12 16:57:39 -0500

    Migraciones
    Se crea migracion de tabla empleados y empresa

---
+++ database/migrations/2021_03_12_211328_create_empleados_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmpleadosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('empleados', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombres',150);
+            $table->string('apellidos',150);
+            $table->integer('empresa_id')->unsigned();
+            $table->string('email',100);
+            $table->string('telefono',50);
+            $table->foreign('empresa_id')
+                ->references('id')
+                ->on('empresas')
+                ->onDelete('restrict');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('empleados');
+    }
+}
